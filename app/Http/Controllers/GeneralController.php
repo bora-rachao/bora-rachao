21fd0b91fe43803f34d62b0c88b32874483956b3
Gabriel Alexandre Ribeiro <gabrielalexandreribeiro750@gmail.com>
2025-11-16 22:05:57 -0300

    feat: privacy policy

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class GeneralController extends Controller
 {
-    // Coloquem aqui as chamadas das Views
+    // Página Privacy Policy
+    public function privacyPolicy()
+    {
+        return view('general.privacy-policy');
+    }
+
+    // Página Terms of Use
+    public function termsOfUse()
+    {
+        return view('general.terms-of-use');
+    }
 }
