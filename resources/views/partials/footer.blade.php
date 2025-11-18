d20394c5115b7d7725424b631ebc5624d74651bd
Beatriz Karoline Monteiro <beatriz.monteiro6@fatec.sp.gov.br>
2025-11-18 09:04:48 -0300

    fix: target blank nos links do footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -33,23 +33,23 @@
             </a>
         </nav>
         <div class="flex justify-center gap-x-4 mt-9">
-            <a href="https://www.instagram.com/"
+            <a href="https://www.instagram.com/" target="_blank"
                 class="bg-pink-500/40 py-1 px-2 rounded-xl text-pink-500 hover:bg-pink-500 duration-300 ease-in-out hover:text-white">
                 <i class="fa-brands fa-instagram text-2xl hover:text-white"></i>
             </a>
-            <a href="https://www.tiktok.com/"
+            <a href="https://www.tiktok.com/" target="_blank"
                 class="bg-stone-500/40 py-1 px-2 rounded-xl text-stone-500 hover:bg-stone-950 duration-300 ease-in-out hover:text-white">
                 <i class="fa-brands fa-tiktok text-2xl"></i>
             </a>
-            <a href="https://www.facebook.com/"
+            <a href="https://www.facebook.com/" target="_blank"
                 class="bg-blue-500/40 py-1 px-2 rounded-xl text-blue-500 hover:bg-blue-500 duration-300 ease-in-out hover:text-white">
                 <i class="fa-brands fa-square-facebook text-2xl"></i>
             </a>
-            <a href="https://x.com/"
+            <a href="https://x.com/" target="_blank"
                 class="bg-stone-600/40 py-1 px-1.5 rounded-xl text-stone-600 hover:bg-stone-950 duration-300 ease-in-out hover:text-white">
                 <i class="fa-brands fa-x-twitter text-2xl"></i>
             </a>
-            <a href="https://www.linkedin.com/"
+            <a href="https://www.linkedin.com/" target="_blank"
                 class="bg-sky-500/40 py-1 px-2 rounded-xl text-sky-500 hover:bg-sky-500 duration-300 ease-in-out hover:text-white">
                 <i class="fa-brands fa-linkedin text-2xl"></i>
             </a>
