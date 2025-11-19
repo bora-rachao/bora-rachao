3ff20deb750dd2149c0e131883af337228ba9b97
lucas-bardeli <lucasbbsjau@gmail.com>
2025-11-19 12:56:55 -0300

    feat: UsersSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Sport;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        $this->call(SportsSeeder::class);
+        if (!Sport::first()) {
+            $this->call(SportsSeeder::class);
+        }
+        if (!User::first()) {
+            $this->call(UsersSeeder::class);
+        }
     }
 }
